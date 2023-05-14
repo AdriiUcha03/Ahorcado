e921f3c3a06844ab6b335b3a48fa2525a1a8ee1a
Adrián Ucha Sousa <adrii.ucha03@gmail.com>
2023-05-14 22:07:06 +0200

    V1

--- ahorcado.py
+++ ahorcado.py
@@ -1,8 +1,9 @@
 import random
 
+
 class juegoAhorcado:
     ESTADOS = [
-    r"""
+        r"""
      +--+
      |  |
         |
@@ -10,23 +11,23 @@ class juegoAhorcado:
         |
         |
     =====""",
-    r"""
-     +--+
-     |  |
-     O  |
-        |
-        |
-        |
-    =====""",
-    r"""
+        r"""
      +--+
      |  |
      O  |
+        |
+        |
+        |
+    =====""",
+        r"""
+     +--+
+     |  |
+     O  |
      |  |
         |
         |
     =====""",
-    r"""
+        r"""
      +--+
      |  |
      O  |
@@ -34,7 +35,7 @@ class juegoAhorcado:
         |
         |
     =====""",
-    r"""
+        r"""
      +--+
      |  |
      O  |
@@ -42,7 +43,7 @@ class juegoAhorcado:
         |
         |
     =====""",
-    r"""
+        r"""
      +--+
      |  |
      O  |
@@ -50,7 +51,7 @@ class juegoAhorcado:
     /   |
         |
     =====""",
-    r"""
+        r"""
      +--+
      |  |
      O  |
@@ -60,7 +61,7 @@ class juegoAhorcado:
     ====="""]
 
     SALVADO = [
-    r"""
+        r"""
      +--+
         |
         |
@@ -70,8 +71,8 @@ class juegoAhorcado:
     ====="""]
 
     C = 'FRUTAS'
-    W = 'PERA PLATANO UVA MANZANA MELOCOTON KIWI ALBARICOQUE CEREZA CIRUELA FRESA GRANADA HIGO LIMA LIMON MANDARINA NARANJA MELON MORA NISPERO PIÑA POMELO SANDIA '.split()
-
+    W = 'PERA PLATANO UVA MANZANA MELOCOTON KIWI ALBARICOQUE CEREZA CIRUELA FRESA GRANADA HIGO LIMA LIMON MANDARINA ' \
+        'NARANJA MELON MORA NISPERO PIÑA POMELO SANDIA '.split()
 
     def jugar(self):
 
@@ -80,14 +81,13 @@ class juegoAhorcado:
         secreto = random.choice(self.W)
 
         while True:
-            self.dibujar(li,lc,secreto)
+            self.dibujar(li, lc, secreto)
 
-            nl = self.DIMELETRA(li+lc)
+            nl = self.DIMELETRA(li + lc)
 
             if nl in secreto:
 
                 lc.append(nl)
-
 
                 g = True
                 for sl in secreto:
@@ -103,16 +103,15 @@ class juegoAhorcado:
             else:
                 li.append(nl)
 
-                if len(li) == len(self.ESTADOS)-1:
-                    self.dibujar(li,lc,secreto)
+                if len(li) == len(self.ESTADOS) - 1:
+                    self.dibujar(li, lc, secreto)
                     print('Demasiados intentos!')
                     print('La palabra era "{}"'.format(secreto))
                     break
 
-
-    def dibujar(self,li,lc,secreto):
+    def dibujar(self, li, lc, secreto):
         print(self.ESTADOS[len(li)])
-        print('La categoría es: ',self.C)
+        print('La categoría es: ', self.C)
         print()
 
         print('Letras incorrectas: ', end='')
@@ -120,16 +119,14 @@ class juegoAhorcado:
             print(let, end=' ')
         if len(li) == 0 and 0 == len(li):
             print('No hay letras incorrectas.')
-        if len(li) == len(li)+1:
+        if len(li) == len(li) + 1:
             print('Letras diferentes.')
         if len(li) == len(li) + 2:
             print('No coinciden.')
 
-
-
         print()
 
-        spa = ['_']*len(secreto)
+        spa = ['_'] * len(secreto)
 
         for i in range(len(secreto)):
             if secreto[i] in lc:
@@ -137,16 +134,15 @@ class juegoAhorcado:
 
         print(' '.join(spa))
 
-
     def DIMELETRA(self, ya):
         while True:
             print('Adivina una letra.')
             adivina = input('> ').upper()
             if len(adivina) != 1:
                 print('Introduce una única letra.')
-            elif adivina  in ya:
+            elif adivina in ya:
                 print('Esa letra ya la sabías. Elige otra vez.')
-            elif not  adivina.isalpha():
+            elif not adivina.isalpha():
                 print('Introduce una LETRA.')
 
             else:
@@ -154,6 +150,5 @@ class juegoAhorcado:
 
 
 if __name__ == '__main__':
-    juego1=juegoAhorcado()
+    juego1 = juegoAhorcado()
     juego1.jugar()
-
